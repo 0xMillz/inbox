3e03ec704842eda0e83dd264757dd9a47c8bcc71
millsmcilroy <millsmcilroy@gmail.com>
2018-02-10 16:47:57 -0500

    56. Project Review

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert'); // included with node standard library
 const ganache = require('ganache-cli');// local ethereum network
 const Web3 = require('web3');// importing web3 constructor, so capital W
-const web3 = new Web3(ganache.provider());//arg is network you are connecting to
+const provider = ganache.provider();
+const web3 = new Web3(provider);//arg is network you are connecting to
 const {interface, bytecode} = require('../compile');
 
 let accounts;
@@ -11,14 +12,27 @@ beforeEach(async () => {
 
     accounts = await web3.eth.getAccounts(); // returns list of unlocked ganache accounts
 
-    // use one of ^ accounts to deploy the contract
+    // use one of these^ accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there!'] })
-        .send({ from: accounts[0], gas: '1000000' })
+        .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
         assert.ok(inbox.options.address);// assert this is this a defined value
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    });
+
+    it('sets the message when setMessage is called', async () => {
+        await inbox.methods.setMessage('New Message!').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'New Message!');
     });
 });
